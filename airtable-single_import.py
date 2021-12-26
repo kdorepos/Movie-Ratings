ac9abea9026dd70c6f5a7a789646775fd2f5ff3a
Kevin Dorsey <kdorsey0718@gmail.com>
2021-12-25 20:48:06 -0500

    Adds comments to code

--- airtable-single_import.py
+++ airtable-single_import.py
@@ -8,21 +8,21 @@ import csv
 # vars
 omdb_apiKey = '4bc136e0'
 
-# starting process
+# parsing arguments
 parser = argparse.ArgumentParser(
     description='Parsing arguments for movie tracking'
 )
-
 parser.add_argument('-m', '--movie-name', metavar='movie_name', required=True)
 parser.add_argument('-y', '--year', metavar='year', required=True)
 parser.add_argument('-r', '--rating', metavar='rating', required=True)
-
 args = parser.parse_args()
 
+# setting vars from arguments
 movieInput = args.movie_name
 movieYear = args.year
 myRating = args.rating
 
+# cleaning up arguments, prepping for OMDB processing
 myRating = str(myRating)
 myRating = re.sub(r'\n', '', myRating)
 fRating = float(myRating)
@@ -33,11 +33,14 @@ url = 'http://www.omdbapi.com/?type=movie&y=' + \
     movieYear + '&t=' + movieName + '&apikey=' + omdb_apiKey
 r = requests.post(url)
 data = r.json()
+
+# setting variables from OMDB
 movieTitle = data['Title']
-# movieTitle = re.sub(r'\'', '\\\'', movieTitle)
 movieYear = data['Year']
 movieDirector = data['Director']
 movieGenre = data['Genre']
+
+# pushing to airtable
 airURL = 'https://api.airtable.com/v0/appPs8NuOpz0Tjb8t/Movie%20Ratings'
 airHeaders = {
     'Authorization': 'Bearer keyQkpTTG6cd7HiBB',
